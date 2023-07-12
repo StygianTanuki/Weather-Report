255bb0256d59bb54ca05f208ab34ffb23415c02a
Stygian Tanuki <136685351+StygianTanuki@users.noreply.github.com>
2023-07-12 01:23:11 -0400

    Added starter code for all files and reorganized

---
+++ assests/js/script.js
@@ -0,0 +1,28 @@
+// 3 global variables
+// 1 for API KEY
+//1 for base url: openweathermap.org
+// search history array
+
+
+// FUNCTION === Display search history
+// loop over history array and count down so the most recent search is at the top
+
+// FUNCITON --- update search history in local storage
+
+// Function --- get search history from local storage
+
+// Function -- display current weather data from fetch request
+
+// fucntions -- display 5 day forcast data from fetch request
+//--- need icon, tempt, humidity, date, wind speet
+
+
+// create elements for each of the above
+
+//create elements for each of the above
+
+// FUNCTION -- display 5 day forcast data from fetch request for each card
+
+// FUNCTION -- specifically for geolocation with lat and long and use fucntion above to make calls here
+
+//FUNCTION -- event listener for each button
