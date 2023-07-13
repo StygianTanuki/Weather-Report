4f7326487be8970a9f7fc4c4b0f37970396f4344
Stygian Tanuki <136685351+StygianTanuki@users.noreply.github.com>
2023-07-13 03:39:49 -0400

    Added more to JS

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,19 +1,23 @@
-var APIKey = "cf9803c3b377a9b6c5550c2755ccbd51"
-var searchButton = document.getElementById("search-btn")
-var cityLocation = document.getElementById("search-city")
+var APIKey = "cf9803c3b377a9b6c5550c2755ccbd51";
+var searchButton = document.getElementById("search-button");
+var cityLocation = document.getElementById("search-city");
+var weatherBlock = document.getElementById("current-weather");
+var fiveDayblock = document.getElementById("five-day");
+var searchHistorycontainer = document.getElementById("search-history");
 
 function search() {
-    var content = cityLocation.value
-    console.log(content)
+    var content = cityLocation.value;
+    console.log(content);
 
-    var url = "api.openweathermap.org/data/2.5/forecast?" + content + "&appid=" + APIKey + "&units=imperial"
+    var url = "https://api.openweathermap.org/data/2.5/forecast?" + content + "&appid=" + APIKey + "&units=imperial";
 
-    console.log(url)
+
+    weatherBlock.innerHTML = "";
+    fiveDayblock.innerHTML = "";
 
     fetch(url)
-
     .then(function(response){
-        return response.json()
+        return response.json();
     })
     .then(function(data){ 
         console.log(data);
@@ -22,17 +26,21 @@ function search() {
         var currentDate = new Date();
         var cityName = data.city.name;
 
-        var cadDiv = document.createElement("div");
+        var cardDiv = document.createElement("div");
         cardDiv.classList.add("card", "mx-auto");
 
 
 
         var cardBody = document.createElement("div");
-        cardBody.classList.add("cardBody");
+        cardBody.classList.add("cardBody", 'bg-success');
 
         var cardTitle = document.createElement("h1");
-        cardTitle.classList.add("cardTitle");
-        cardTitle.textContent = "Current Date: " + currentDate.toDateString();
+        cardTitle.classList.add("card-title");
+        var icon = document.createElement("i");
+        icon.classList.add("fas", "fa-calendar-alt");
+
+        var currentDateText = document.createElement("span");
+        currentDateText.textContent = "Current Date: " + currentDate.toDateString();
 
         var cityNametitle = document.createElement("h2");
         cityNametitle.textContent = "City: " + cityName;
@@ -57,8 +65,8 @@ function search() {
 
         cardBody.appendChild(cardBody);
 
-        var weatherForcastContainer = document.getElementById("weatherForcast");
-        currentDay.appendChild(cardDiv);
+        cardDiv.appendChild(cardBody);
+        weatherBlock.appendChild(cardDiv);
 
         var fiveDay = [
             data.list[1],
@@ -72,41 +80,106 @@ function search() {
 
         for(i = 0; i < fiveDay.length; i++) {
             var sectionBlock = document.createElement("div");
-            var d
+            sectionBlock.classList.add("card", "col");
+            var dateH3 = document.createElement("h3");
+            dateH3.textContent = formatDateString(fiveDay[i].dt_txt);
+
+
+            var ulNew = document.createElement("ul");
+            ulNew.classList.add("list-group");
+            ulNew.classList.add("list-group-flush");
+
+            var tempLi = document.createElement("li");
+            tempLi.textContent = "Temp: " + fiveDay[i].main.temp;
+            tempLi.classList.add("list-group-item");
+
+            var windLi = document.createElement("li");
+            windLi.textContent = "Wind Speed: " + fiveDay[i].main.wind;
+            windLi.classList.add("list-group-item");
+
+            var humidityLi = document.createElement("li");
+            humidityLi.textContent = "Humidity: " + fiveDay[i].main.humidity;
+            humidityLi.classList.add("list-group-item");
+
+            ulNew.appendChild(tempLi);
+            ulNew.appendChild(windLi);
+            ulNew.appendChild(humidityLi);
+
+            sectionBlock.appendChild(dateH3);
+            sectionBlock.appendChild(ulNew);
+
+            fiveDayblock.appendChild(sectionBlock);
         }
-    })
+
+        localCity(content);
+        searchHistory();
+    });
 }
 
-searchButton.addEventListener("click", search)
+    function getWeatherIconClass(iconCode) {
+        var iconMap = {
+          "01d": "fa-sun",
+          "02d": "fa-cloud-sun",
+          "03d": "fa-cloud",
+          "04d": "fa-cloud",
+          "09d": "fa-cloud-showers-heavy",
+          "10d": "fa-cloud-rain",
+          "11d": "fa-bolt",
+          "13d": "fa-snowflake",
+          "50d": "fa-smog",
+          "01n": "fa-moon",
+          "02n": "fa-cloud-moon",
+          "03n": "fa-cloud",
+          "04n": "fa-cloud",
+          "09n": "fa-cloud-showers-heavy",
+          "10n": "fa-cloud-rain",
+          "11n": "fa-bolt",
+          "13n": "fa-snowflake",
+          "50n": "fa-smog",
+        };
+      
+        return iconMap[iconCode];
+    }
 
-// 3 global variables
-// 1 for API KEY
-//1 for base url: openweathermap.org
-// search history array
+    function formatDateString(dateString) {
+        var date = new Date(dateString);
+        var formattedDate = date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+        return formattedDate;
+    }
 
+    function localCity(city) {
+        var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        searchHistory.unshift(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    }
 
-// FUNCTION === Display search history
-// loop over history array and count down so the most recent search is at the top
+    function displaySearch() {
+        var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+        var searchHistorycontainer = document.getElementById("search-history");
+        searchHistorycontainer.innerHTML = "";
 
-// FUNCITON --- update search history in local storage
+        for (var i = 0; i < searchHistory.length; i++) {
+            var cityDiv = document.createElement("div");
+            cityDiv.classList.add("searched-city");
+            cityDiv.textContent = searchHistory[i];
 
-// Function --- get search history from local storage
+            cityDiv.addEventListener("click", function (event) {
+                var cityName = event.target.textContent;
+                searchCity.value = cityName;
+                search();
+            });
 
-// Function -- display current weather data from fetch request
+            cityDiv.addEventListener("mouseenter", function (event) {
+                event.target.classList.add("hovered-city");
+            });
 
-// fucntions -- display 5 day forcast data from fetch request
-//--- need icon, tempt, humidity, date, wind speet
+            cityDiv.addEventListener("mouseleave", function (event) {
+                event.target.classList.remove("hovered-city");
+            });
+        
+            searchHistorycontainer.appendChild(cityDiv);
 
-function getApi() {
-    var apiURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={cf9803c3b377a9b6c5550c2755ccbd51}"
-}
+        }
+    }
 
-// create elements for each of the above
-
-//create elements for each of the above
-
-// FUNCTION -- display 5 day forcast data from fetch request for each card
-
-// FUNCTION -- specifically for geolocation with lat and long and use fucntion above to make calls here
-
-//FUNCTION -- event listener for each button
+searchButton.addEventListener("click", search);
